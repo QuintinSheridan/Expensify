6dc955d53cf98cdeb49c968d47034ed65f149324
Quintin Sheridan <qsherida@alumni.nd.edu>
2020-10-26 17:54:21 -0600

    dev and prod dependencies separated

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ app.get('*', (request, response) => {
     response.sendFile(path.join(publicPath, 'index.html'))
 })
 
-// get herku port from environment variables
-const port = process.env.PORT;
+// get herku port from environment variables, use 3000 for local dev
+const port = process.env.PORT||3000;
 app.listen(port , () => {
     console.log(`Server is up and running on port ${port}`)
 })
